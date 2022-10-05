5e78439e36e12691e5360c841eeab93782f08664
eric-cub <eric.lochner@juventus.schule>
2022-10-05 18:53:29 +0200

    Update main.c
    Einfügen von Leibniz Formel.
    
    Update main.c
    
    AVOR Event Group

--- U_PiCalc_HS2022/main.c
+++ U_PiCalc_HS2022/main.c
@@ -2,7 +2,7 @@
  * U_PiCalc_HS2022.c
  *
  * Created: 20.03.2018 18:32:07
- * Author : -
+ * Author : Eric Lochner
  */ 
 
 #include <math.h>
@@ -32,6 +32,19 @@
 
 
 void controllerTask(void* pvParameters);
+void leibniztask(void* pvParameters);
+
+float piCalculatet;
+
+//EventGroup for ButtonEvents.
+EventGroupHandle_t egButtonEvents = NULL;
+#define BUTTON1_SHORT	0x01 //Startet Algorithmus
+#define BUTTON2_SHORT	0x02 //Stoppt Algorithmus
+#define BUTTON3_SHORT	0x04 //Zurücksetzen des Algorithmus
+#define BUTTON4_SHORT	0x08 //Für Zustand un wechselvom Algorithmus
+#define BUTTON_ALL		0xFF //Reset 
+
+
 
 int main(void)
 {
@@ -39,6 +52,8 @@ int main(void)
 	vInitDisplay();
 	
 	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL);
+	xTaskCreate( leibniztask, (const char *) "leibniz_task", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
+
 
 	vDisplayClear();
 	vDisplayWriteStringAtPos(0,0,"PI-Calc HS2022");
@@ -47,35 +62,52 @@ int main(void)
 	return 0;
 }
 
+
+//Modes for Finite State Machine
+#define MODE_IDLE 0
+#define MODE_Leibnitz 1
+#define MODE_SecondAlgorythm 2
+#define MODE_ALARMALARM 3
+
+
+
+
+		void leibniztask(void* pvParameters) {
+			float pi4 = 1;
+			float pi = 0;
+			uint32_t n = 3;
+			for(;;){
+				pi4 = pi4 -1.0/n + 1.0/(n+2);
+				n = n+4;
+				piCalculatet = pi4 * 4;
+			}
+		}
+		
+		
+		
 void controllerTask(void* pvParameters) {
+	egButtonEvents = xEventGroupCreate();
 	initButtons();
 	for(;;) {
 		updateButtons();
 		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
 			char pistring[12];
-			sprintf(&pistring[0], "PI: %.8f", M_PI);
+			sprintf(&pistring[0], "PI: %.8f", piCalculatet);
 			vDisplayWriteStringAtPos(1,0, "%s", pistring);
 		}
+		updateButtons();
+		
+		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
+			xEventGroupSetBits(egButtonEvents, BUTTON1_SHORT);
+		}
 		if(getButtonPress(BUTTON2) == SHORT_PRESSED) {
-			
+			xEventGroupSetBits(egButtonEvents, BUTTON2_SHORT);
 		}
 		if(getButtonPress(BUTTON3) == SHORT_PRESSED) {
-			
+			xEventGroupSetBits(egButtonEvents, BUTTON3_SHORT);
 		}
 		if(getButtonPress(BUTTON4) == SHORT_PRESSED) {
-			
-		}
-		if(getButtonPress(BUTTON1) == LONG_PRESSED) {
-			
-		}
-		if(getButtonPress(BUTTON2) == LONG_PRESSED) {
-			
-		}
-		if(getButtonPress(BUTTON3) == LONG_PRESSED) {
-			
-		}
-		if(getButtonPress(BUTTON4) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(egButtonEvents, BUTTON4_SHORT);
 		}
 		vTaskDelay(10/portTICK_RATE_MS);
 	}
